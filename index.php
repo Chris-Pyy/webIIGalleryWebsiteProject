e34d194b0e865ba95df4120389411424bc17d4ca
Taylor <taylor.amy101@gmail.com>
2021-11-19 11:25:57 -0700

    updated navbar and started single city page

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="styles/search.css">
     <script src="javascript/navbar.js"></script>
     <!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <title>Document</title>
 </head>
 
