4135e65d9442c759355f1b96b4e891ba937e1414
Chris-Pyy <55000165+Chris-Pyy@users.noreply.github.com>
2021-11-21 22:27:10 -0700

    added country filters+list to City Page

--- single-city.php
+++ single-city.php
@@ -34,6 +34,9 @@ if (isset($_GET['id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/navbar.css">
     <link rel="stylesheet" href="styles/styles.css">
+    <script src="javascript/countryFilters.js"></script>
+    <script src="javascript/countryList.js"></script>
+    <script src="javascript/images.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -43,11 +46,51 @@ if (isset($_GET['id'])) {
 <body>
     <?php include 'navbar.inc.php'; ?>
     <div class='row'>
-        <div class="col s12 m12 l3 column center-align card-panel z-depth-5 main">
-            <div class="row s1 card-panel grey lighten-4 z-depth-1 hoverable">
-                <p>filters</p>
+    <div class="col s12 m12 l3 column main">
+            <div class="row s1 card-panel">
+                <p class="center-align">filters</p>
+                <div class="input-field col s12">
+                    <input id="filter_search" type="text">
+                    <label class="blue-text" for="filter_search">search Countries</label>
+                </div>
+                <button id="filter_imgavail" class='waves-effect waves-light btn blue signup col s12'>images available</button>
+                <div id="filter_continent" class="col card-panel">
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>Africa</span>
+                    </label>
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>Antarctica</span>
+                    </label>
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>Asia</span>
+                    </label>
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>Europe</span>
+                    </label>
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>North America</span>
+                    </label>
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>Oceania</span>
+                    </label>
+                    <label class="col s12">
+                        <input type="checkbox" class="filled-in filterCheckbox" />
+                        <span>South America</span>
+                    </label>
+                </div>
             </div>
-                <p>filter list</p>
+            <div class="row s1 card-panel" style="overflow: auto; height: 40vh;">
+                <p class="center-align">Country list</p>
+                <div class="row countryList center-align">
+                </div>
+            </div>
+                
         </div>
         <div class="col s12 m12 l6 column center-align card-panel main">
             <div class="row s1 card-panel">
