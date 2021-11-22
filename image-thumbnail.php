feb1fd5462d020fdfd7873100e94cef18867cc56
Chris-Pyy <55000165+Chris-Pyy@users.noreply.github.com>
2021-11-21 21:45:59 -0700

    commit

---
+++ image-thumbnail.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class ImageThumbnail {
+
+    private $image; 
+
+    public function __construct($image) {
+        $this->image = $image;
+    }
+
+    public function createElement() {
+        $fontSize = '14';
+        $titleLength = strlen($this->image['Title']);
+         
+
+        // if the title is longer than 25 characters, reduce the font size. If it is longer than 34 characters, reduce it further.
+        if ($titleLength >= 24) {
+            $fontSize = '12';
+        }
+        if ($titleLength >= 34) {
+            $fontSize = '11.5';
+        }
+
+
+        return '<div class="card col s3 hoverable">
+        <div class="card-image">
+        <img src="https://res.cloudinary.com/taylor-amy/image/upload/c_scale,h_75,w_75/v1633386733/web%202%20asg%201/'. $this->image['Path'] .'">
+        </div>
+        <div class="card-content">
+        <p class="imageTitle" style="font-size: ' . $fontSize. 'px;">'. $this->image['Title'] .'</p>
+        </div>
+        </div>';
+    }
+
+
+
+
+}
+
+
+
